7ffa1d102275c415c1090b89a5117a532822b56a
Reece Boyd <reece.boyd@instacart.com>
2020-05-19 12:47:57 -0500

    define resolver

--- index.js
+++ index.js
@@ -30,3 +30,11 @@ const books = [
     author: "Michael Crichton",
   },
 ];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
